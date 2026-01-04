392e6b053b4c4553eab91a77e2a447677b566625
yagam1light <neverloseyournumber@gmail.com>
2026-01-04 19:47:55 +0100

    Update functions.php

--- demo-theme/functions.php
+++ demo-theme/functions.php
@@ -3,11 +3,21 @@
         wp_enqueue_style('demo-style', get_stylesheet_uri());
     }
     add_action('wp_enqueue_scripts', 'demo_theme_assets');
+
     function demo_theme_setup() {
+
+    add_theme_support('title-tag');
+    add_theme_support('post-thumbnails');
+
+    add_theme_support('editor-styles');
+    add_theme_support('align-wide');
+    add_editor_style('editor-style.css');
+
     register_nav_menus(array(
-        'left-menu'  => __('Left Menu', 'demo-theme'),
-        'footer-menu' => __('Footer Menu', 'demo-theme')
+        'left-menu'   => __('Left Menu', 'demo-theme'),
+        'footer-menu' => __('Footer Menu', 'demo-theme'),
     ));
-}
-    add_action('after_setup_theme', 'demo_theme_setup');
+    }
+    add_action('after_setup_theme', 'demo_theme_setup');;
+
 
